291f51377e95ea1808b3079f0e69b27be589d9cb
Mark Corbett <37854406+speleonut@users.noreply.github.com>
2024-04-23 15:13:24 +0930

    remove skylake

--- collate.trhist.files.phoenix.sh
+++ collate.trhist.files.phoenix.sh
@@ -2,7 +2,7 @@
 
 #SBATCH -J tableTRhist
 #SBATCH -o /hpcfs/users/%u/log/collateTRhist.slurm-%j.out
-#SBATCH -p skylake,icelake,a100cpu
+#SBATCH -p icelake,a100cpu
 #SBATCH -N 1
 #SBATCH -n 2
 #SBATCH --time=01:00:00
